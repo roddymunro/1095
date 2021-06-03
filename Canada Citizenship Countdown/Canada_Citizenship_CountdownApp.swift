7297e98b7436bd0ddd837ce08109becf0d5f485b
Roddy Munro <roddymunro@icloud.com>
2021-06-03 11:11:45 -0300

    Initial Commit

---
+++ Canada Citizenship Countdown/Canada_Citizenship_CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Canada_Citizenship_CountdownApp.swift
+//  Canada Citizenship Countdown
+//
+//  Created by Roddy Munro on 2021-06-03.
+//
+
+import SwiftUI
+
+@main
+struct Canada_Citizenship_CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
